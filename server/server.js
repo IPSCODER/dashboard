3e0628c9f64d74317f4f4b205e7c9bf2851cf1fe
Pravin Sawant <88920922+IPSCODER@users.noreply.github.com>
2024-03-27 09:23:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://dashboard-gold-nu-95.vercel.app/"],
     methods:["POST","GET"],
     credentials:true
 }));
