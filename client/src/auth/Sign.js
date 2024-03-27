c4884df5e1d20be60538e3ac0aea411194837d40
Pravin Sawant <88920922+IPSCODER@users.noreply.github.com>
2024-03-27 09:25:33 +0530

    Update Sign.js

--- client/src/auth/Sign.js
+++ client/src/auth/Sign.js
@@ -55,7 +55,7 @@ const Sign = () => {
 				setErrorMessage('Invalid password(At least 8 characters, one uppercase, one lowercase, one number)');
 				return;
 			  }else{
-				await axios.post('http://localhost:5000/user/signin',{email,password}).then((resp)=>{
+				await axios.post('https://dbackend-peach.vercel.app/user/signin',{email,password}).then((resp)=>{
 				if (resp.data.result == true) {
 					navigate("/")
 					dispatch(loadUser(email))
@@ -82,7 +82,7 @@ const Sign = () => {
 				setErrorMessage('Invalid password(At least 8 characters, one uppercase, one lowercase, one number)');
 				return;
 			  }else{
-				await axios.post('http://localhost:5000/user/signup',{username,email,password}).then((resp)=>{
+				await axios.post('https://dbackend-peach.vercel.app/user/signup',{username,email,password}).then((resp)=>{
 				if (resp.data.result == true) {
 					navigate("/")
 					dispatch(loadGetData())
